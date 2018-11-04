1d6eeebee79704907e53be230ebecf21047e90b6
Kyle Kramer <krkyle@umich.edu>
2018-11-04 15:30:33 -0500

    2 min timeout run.py

--- run.py
+++ run.py
@@ -78,6 +78,7 @@ with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
     output = StreamingOutput()
     # Uncomment the next line to change your Pi's Camera rotation (in degrees)
     camera.rotation = 180
+    picamera.PiCamera.CAPTURE_TIMEOUT = 120
     camera.start_recording(output, format='mjpeg')
     try:
         address = ('', 8000)
