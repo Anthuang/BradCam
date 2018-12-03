d57c4229387294a7111dd5f6c2b2953a7d7293e6
Anthony Huang <anthuang@umich.edu>
2018-12-03 12:34:13 -0500

    Camera rotation

--- run.py
+++ run.py
@@ -75,8 +75,7 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
 with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
     output = StreamingOutput()
     # Uncomment the next line to change your Pi's Camera rotation (in degrees)
-    camera.rotation = 180
-    picamera.PiCamera.CAPTURE_TIMEOUT = 120
+    camera.rotation = 0
     camera.start_recording(output, format='mjpeg')
     try:
         address = ('', 8000)
